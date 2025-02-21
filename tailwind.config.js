4099c17b26249c605f0b41bccd30273d076553a3
agha-naveed <naveedabs31@gmail.com>
2025-02-21 17:23:59 +0500

    ....

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,10 @@ export default {
       colors: {
         bgClr1: "#149957"
       },
+      fontSize: {
+        'heading-xl': "max(5.125rem,min(6.3333vw + 1rem,11rem))",
+        'heading-lg': "max(5.125rem,min(5vw + 1rem,11rem))"
+      }
     },
   },
   plugins: [],
