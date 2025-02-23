296b688464d22b35250f51f99642f7de4a39592f
agha-naveed <naveedabs31@gmail.com>
2025-02-23 10:59:58 +0500

    .

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -6,36 +6,58 @@ export default function Footer() {
   return (
     <>
         <div className='container mx-auto text-white relative py-32'>
-            <div className='flex justify-center gap-10'>
+            <div className='flex justify-center gap-16'>
                 <div className='w-56'
                   data-aos="fade-left"
-                  data-aos-offset="3000"
+                  data-aos-offset="300"
                   data-aos-duration="1000"
                 >
                     <img src={zeroSugar} className='w-full' alt=""  />
                 </div>
 
 
-                <div className='flex flex-col'>                
+                <div className='flex flex-col gap-2 justify-center w-[480px]'>
+
                     <div
-                      className='h-fit'
+                      className='h-fit w-fit'
                       data-aos="fade-up"
-                      data-aos-offset="3000"
+                      data-aos-offset="300"
                       data-aos-duration="1000" 
                     >
-                        <h2 className='text-4xl font-bold'>7UP Zero Sugar</h2>
+                        <h2 className='text-4xl font-bold w-fit'>7UP Zero Sugar</h2>
+                    </div>
+
+                    <div 
+                      data-aos="fade-up"
+                      data-aos-offset="300"
+                      data-aos-duration="1000" 
+                      className='text-2xl flex'>
+                        <IoIosStar className='text-orange-400' />
+                        <IoIosStar className='text-orange-400' />
+                        <IoIosStar className='text-orange-400' />
+                        <IoIosStar className='text-gray-400' />
+                        <IoIosStar className='text-gray-400' />
                     </div>
 
                     <div
                       className='h-fit'
                       data-aos="fade-up"
-                      data-aos-offset="3000"
+                      data-aos-offset="300"
                       data-aos-duration="1000" 
                     >
-                        <p className='text-xl text-right'>
+                        <p className='text-xl'>
                             Feel refreshed by the balanced taste of 7UP® Zero Sugar.
                         </p>
                     </div>
+
+                    <button className='border border-white rounded-xl py-[10px] px-5 transition-all hover:bg-white hover:text-green-800 w-fit mt-6'
+                      data-aos="fade-up"
+                      data-aos-offset="100"
+                      data-aos-duration="1500"
+                    >
+                        Nutrition Facts
+                    </button>
+
                 </div>
 
             </div>
